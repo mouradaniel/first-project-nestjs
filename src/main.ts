a6a4ef2798b1118d52764d6cbc6b49cabf65b04a
Daniel Moura <daniel-moura@outlook.com>
2023-01-29 14:05:00 -0300

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('First Project NestJS')
+    .setDescription(
+      'This API was created in order to study the NestJS framework.',
+    )
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
