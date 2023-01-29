adb448e731843479f91d4e6cc51e69448cf31cb8
Daniel Moura <daniel-moura@outlook.com>
2023-01-29 14:06:59 -0300

    feat: implement on contract find all, find one, update and remove

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,3 +1,13 @@
+import { Players } from '@prisma/client';
+import { UpdatePlayerDto } from 'src/dtos/update-player';
+
 export abstract class PlayersRepository {
   abstract create(username: string, email: string): Promise<void>;
+  abstract findAll(): Promise<Players[]>;
+  abstract findOne(id: string): Promise<Players>;
+  abstract update(
+    id: string,
+    updatePlayerDto: UpdatePlayerDto,
+  ): Promise<Players>;
+  abstract remove(id: string): Promise<any>;
 }
