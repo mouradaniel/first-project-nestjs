237c39ac4e84e2a331cb825626bc88b9fd7443e6
Daniel Moura <daniel-moura@outlook.com>
2023-01-30 15:07:43 -0300

    feat: add password on model players

--- prisma/migrations/20230130173715_create/migration.sql
+++ prisma/migrations/20230130173715_create/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "Players" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Players_pkey" PRIMARY KEY ("id")
