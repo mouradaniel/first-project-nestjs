d8d110b6058e2c202a428c700a65c1398a57e41d
Daniel Moura <daniel-moura@outlook.com>
2023-01-30 18:13:52 -0300

    feat: config env and import auth module on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [ConfigModule.forRoot(), PlayersModule, AuthModule],
 })
 export class AppModule {}
